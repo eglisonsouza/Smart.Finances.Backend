9bc0112cda995e3e707929ba67af52ecf9cda765
eglisonsouza <79208618+eglisonsouza@users.noreply.github.com>
2023-10-26 14:33:35 -0300

    Add project files.

---
+++ Smart.Finances.DataAccess.Core/Common/Events/IRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Smart.Finances.DataAccess.Core.Common.Events
+{
+    public interface IRequestHandler<in TRequest>
+    {
+        Task Handle(TRequest request);
+    }
+
+    public interface IRequestHandler<in TRequest, TResponse>
+    {
+        Task<TResponse> Handle(TRequest requset);
+    }
+}
