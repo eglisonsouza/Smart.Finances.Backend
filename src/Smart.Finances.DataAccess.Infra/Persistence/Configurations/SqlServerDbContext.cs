2acef6c322974c6a203c9aeee11431c73556aabc
eglisonsouza <79208618+eglisonsouza@users.noreply.github.com>
2023-10-27 11:25:30 -0300

    feat: adicionado conexão com banco de dados

---
+++ src/Smart.Finances.DataAccess.Infra/Persistence/Configurations/SqlServerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Smart.Finances.DataAccess.Infra.Persistence.Configurations
+{
+    public class SqlServerDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SqlServerDbContext).Assembly);
+        }
+    }
+}
---
+++ src/Smart.Finances.DataAccess.IoC/DependencyInjectionAPIExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Smart.Finances.DataAccess.Infra.Persistence.Configurations;
+
+namespace Smart.Finances.DataAccess.IoC
+{
+    public static class DependencyInjectionAPIExtension
+    {
+        public static IServiceCollection AddInfraestructureApi(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration.GetConnectionString("SqlServerConnection");
+            services.AddDbContext<SqlServerDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlServerConnection"),
+                b => b.MigrationsAssembly(typeof(SqlServerDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
